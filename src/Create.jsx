d2526a1bac42ce5231cfefa57c576d71cfe04360
hmDev27 <hmdev27@gmail.com>
2025-07-20 20:43:36 +0630

    Initial commit

---
+++ src/Create.jsx
@@ -0,0 +1,24 @@
+//IMPORT
+import React from "react";
+import "./App.css";
+import {useState} from "react";
+import axios from 'axios'
+
+export default function Create() {
+  const [task, setTask] = useState(); //COMPONENT STATE
+  const handleAdd = () => {
+        axios.post('http://localhost:3001/add', {task: task})
+        .then(result => {
+          location.reload()
+        })
+        .catch(err => console.log(err))
+  }
+  return (
+    <div className="create_form">
+      <input type="text" placeholder="Enter Task" onChange={(e) => setTask(e.target.value)}/>
+      <button type="button" onClick={handleAdd}>Add</button>
+    </div>
+  );
+}
+
+
