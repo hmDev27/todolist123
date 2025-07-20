af84e1a19d23519daca442e554b3b7eb59ee7bd8
hmDev27 <hmdev27@gmail.com>
2025-07-20 21:45:12 +0630

    Your commit message here

--- src/Create.jsx
+++ src/Create.jsx
@@ -8,7 +8,7 @@ export default function Create() {
   const [task, setTask] = useState(); //COMPONENT STATE
   const handleAdd = () => {
         // ✅ Live backend hosted on Cyclic
-axios.post('https://todo-api-xyz.cyclic.app/add', { task: task })
+axios.post('https://80678df4-37e9-4336-8513-a81faa7c7086-00-r7668woiy6sm.sisko.replit.dev/add', { task: task })
 
         .then(result => {
           location.reload()
